82f684901d5a6e30ca1d6776a940ff6aa8d03683
Remo Giermann <mo@liberejo.de>
2011-03-01 23:17:05 +0100

    first lines written, including first algorithm

--- main.c
+++ main.c
@@ -41,3 +41,40 @@
 */
 
 #include <avr/io.h>
+#include <math.h>
+#include "softuart.h"
+
+#define Ts 0.016
+#define fs 1/Ts
+#define PWM OCR1B
+#define ADCHAN ADC1
+
+float Kp = 0;
+float Ki = 0;
+float Kd = 0;
+uint8_t w = 0;
+
+float y = 0;
+uint8_t x = 0;
+float e = 0;
+float e_sum = 0;
+
+void init_wdt();
+void init_adc();
+void init_pwm();
+
+void output();
+uint8_t read_pv();
+
+void contr()
+{
+    float e_last = e;
+
+    x = read_pv();
+    e = w - x;
+    e_sum += e;
+
+    y  = Kp * e;
+    y += Ki * Ts * e_sum;
+    y += Kd * fs * (e - e_last);
+}
