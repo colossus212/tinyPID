a2d725090e7e59875f8d3ae81293b2b5d84bc4fd
Remo Giermann <mo@liberejo.de>
2011-03-11 15:24:20 +0100

    minor change

--- tools.py
+++ tools.py
@@ -11,29 +11,30 @@ def debug(pid):
 	# receives: sign, 2B esum, pterm-sign, 4B pterm, sign, iterm, sign, dterm
 	# sign is 1 for a following negativ value, otherwise 0
 	
-	if len(e) < 18:
-		print e
-		return
-		
-	e = map(ord, e)
-
-	dword = lambda a,b,c,d: (to_word(a, b)<<16)+to_word(c, d)
-	sign  = lambda s, v: s and -v or v
-
-	esign, psign, isign, dsign = e[0], e[3], e[8], e[13]
-	
-	esum  = sign(esign, to_word(e[1], e[2]))
-	pterm = sign(psign, dword(e[4], e[5], e[6], e[7]))
-	iterm = sign(isign, dword(e[9], e[10], e[11], e[12]))
-	dterm = sign(dsign, dword(e[14], e[15], e[16], e[17]))
-
 	print "w: %3i x: %3i e: %3i y: %3i" % (pid.w, pid.x, pid.e, pid.y)
-	print "---------------------------"
-	print " esum: %4i" % esum
-	print "pterm: %4i (unscaled: %3i)" % (pterm, pterm/SCALING_FACTOR)
-	print "iterm: %4i (unscaled: %3i)" % (iterm, iterm/SCALING_FACTOR)
-	print "dterm: %4i (unscaled: %3i)" % (dterm, dterm/SCALING_FACTOR)
-	print "---------------------------"
+	
+	if len(e) == 18:
+		e = map(ord, e)
+
+		dword = lambda a,b,c,d: (to_word(a, b)<<16)+to_word(c, d)
+		sign  = lambda s, v: s and -v or v
+
+		esign, psign, isign, dsign = e[0], e[3], e[8], e[13]
+		
+		esum  = sign(esign, to_word(e[1], e[2]))
+		pterm = sign(psign, dword(e[4], e[5], e[6], e[7]))
+		iterm = sign(isign, dword(e[9], e[10], e[11], e[12]))
+		dterm = sign(dsign, dword(e[14], e[15], e[16], e[17]))
+
+		print "---------------------------"
+		print " esum: %4i" % esum
+		print "pterm: %4i (unscaled: %3i)" % (pterm, pterm/SCALING_FACTOR)
+		print "iterm: %4i (unscaled: %3i)" % (iterm, iterm/SCALING_FACTOR)
+		print "dterm: %4i (unscaled: %3i)" % (dterm, dterm/SCALING_FACTOR)
+		print "---------------------------"
+	else:
+		print e
+	
 	print pid.opmode == 'm' and 'Manual mode.' or 'Auto mode.'
 
 
