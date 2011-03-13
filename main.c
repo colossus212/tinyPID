98474303f7df13dfc9a4932ac8be3df1e767d92a
Remo Giermann <mo@liberejo.de>
2011-03-13 15:06:36 +0100

    updated algo description in main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 /*
+ 
    tinyPID
 
    Implementing a discrete PID Controller on an ATtiny AVR.
@@ -10,34 +11,29 @@
    The microcontroller uses the equation
 
    y = Kp * e + Ki * Ts * e_sum + Kd/Ts * (e - e_last)
-
-   with y being the output value
-   the actual output is done via PWM and ranges from 0 to 255
-
-   Kp, Ki and Kd being the control parameters set by the user
    
-   Ts being the sampling time, hardcoded in the program, 16ms
-
-   e being the error, e = w - x
-   e_sum - accumulated error
-   e_last - error of last sample
-
-   w being the set value, given by the user
-   x being the process value, read via built-in ADC in 8bit mode
-   ranging from 0 to 255.
-
-
-   User interaction is done via software UART.
-
-
-   Short info on parameters:
+   (y - output value,
+    Kp - proportional factor
+    Ki - integral factor
+	Kd - derivative factor
+	e  - error = setpoint - process value
+	e_sum - sum of all last errors
+	e_last - last error
+	Ts - sampling time)
    
-   Kp is the proportional factor
-   Ki is the integrating factor
-   Kd is the differential factor
+   to make things more robust, simpler and to avoid floating point arithmetic,
+   the actual algorithm uses:
    
-   Ki = Kp/Tn
-   Kd = Kp/Td
+   u = P_factor * e + I_factor * e_sum + D_factor * (x - x_last)
+   y = u / SCALING_FACTOR
+   
+   (P_factor - Kp*SCALING_FACTOR
+    I_factor - Ki*Ts*SCALING_FACTOR
+    D_factor - Kd/Ts * SCALING_FACTOR
+    x - process value)
+    
+    It's possible to limit the input (process value) and output value.
+   
 
 */
 
