459b61cf898a0843b3fbeafbbeaf81f6aba7f628
Remo Giermann <mo@liberejo.de>
2011-03-16 23:33:21 +0100

    GUI: improvements in plotter, almost usable

--- gui/tinyPIDqt/scribble/graphtest.py
+++ gui/tinyPIDqt/scribble/graphtest.py
@@ -9,44 +9,31 @@ import numpy
 
 redpen = QPen()
 redpen.setBrush(Qt.red)
-redpen.setWidth(1)
+redpen.setWidth(2)
 redpen.setJoinStyle(Qt.RoundJoin)
 redpen.setCapStyle(Qt.RoundCap)
 
 bluepen = QPen()
 bluepen.setBrush(Qt.blue)
-bluepen.setWidth(1)
+bluepen.setWidth(2)
 bluepen.setJoinStyle(Qt.RoundJoin)
 bluepen.setCapStyle(Qt.RoundCap)
 
 greenpen = QPen()
 greenpen.setBrush(Qt.green)
-greenpen.setWidth(1)
+greenpen.setWidth(2)
 greenpen.setJoinStyle(Qt.RoundJoin)
 greenpen.setCapStyle(Qt.RoundCap)
 
 blackpen = QPen()
 blackpen.setWidth(3)
-blackpen.setBrush(Qt.black)
+blackpen.setBrush(QColor(70,70,70))
 blackpen.setJoinStyle(Qt.RoundJoin)
 blackpen.setCapStyle(Qt.RoundCap)
 
-
-class Dot (QGraphicsEllipseItem):
-	
-	def __init__(self, x, y, w=2, h=2, *args, **kwargs):
-		super(Dot, self).__init__(*args, **kwargs)
-		
-		
-		b = QGraphicsBlurEffect()
-		b.setBlurRadius(1)
-		self.setGraphicsEffect(b)
-	
-	def center(self):
-		x, y = self.x(), self.y()
-		r = self.boundingRect()
-		w, h = r.width(), r.height()
-		self.setPos(x-w/2., y-w/2.)
+thinpen = QPen()
+thinpen.setWidth(0)
+thinpen.setBrush(QColor(100,100,100))
 
 
 class Curve (QGraphicsPathItem):
@@ -77,13 +64,11 @@ class Curve (QGraphicsPathItem):
 	
 	
 	def cropLeft(self, x):
-		if self.points[0].x() < x:
-			self.points = self.points[1:]
-	
+		self.points = [p for p in self.points if p.x() > x]
+			
 	
 	def cropRight(self, x):
-		if self.points[0].x() > x:
-			self.points = self.points[1:]
+		self.points = [p for p in self.points if p.x() < x]
 		
 	
 	def count(self):
@@ -97,43 +82,107 @@ class Curve (QGraphicsPathItem):
 
 class Plotter (QGraphicsScene):
 	
-	def __init__(self, update=250, *args, **kwargs):
+	def __init__(self, update=500, stretch=1, *args, **kwargs):
 		super(Plotter, self).__init__(*args, **kwargs)
 		
+		self.curves = {}
+		self.dots = []
+		self.stamps = []
+		self.ylines = []
+		
 		self.t = 0
+		self.time = 0
 		self.t0 = time.time()
+		self.stretch = stretch
 		
 		self.timer = QTimer()
 		self.timer.setInterval(update)
 		self.timer.start()
 		
-		self.connect(self.timer, SIGNAL("timeout()"), self.__feed)
-		self.connect(self.timer, SIGNAL("timeout()"), self.time)
-	
-		self.curves = {}
-		self.point = (2,2,blackpen)
+		self.stamptimer = QTimer()
+		self.stamptimer.setInterval(1000)
+		self.stamptimer.start()
+		self.makeStamp()
 		
-		
+		self.connect(self.timer, SIGNAL("timeout()"), self.__update)
+		self.connect(self.stamptimer, SIGNAL("timeout()"), self.makeStamp)
+	 
+	 
 	def __write(self, curve, y):
 		curve.addPoint(QPointF(self.t, y))
-		curve.cropLeft(self.views()[0].mapToScene(0,0).x())
-		self.views()[0].centerOn(self.t, y)
 		
 	
-	def __feed(self):
-		for v in self.curves.values():
-			if v.points:
-				last = v.points[-1].y()
-				self.__write(v, last)
+	def __update(self):
+		view = self.views()[0]
+		corner = view.mapToScene(0,0)
+		
+		self.time = time.time() - self.t0
+		self.t = self.time*self.stretch
+				
+		view.centerOn(self.t/2, 0)
+				
+		for curve in self.curves.values():
+			if curve.points:
+				last = curve.points[-1]
+				#self.__write(curve, last.y())
+				curve.cropLeft(corner.x())
+				print len(curve.points),
+				
+		for i in range(len(self.dots)):
+			if i < len(self.dots):
+				if self.dots[i].boundingRect().x() < corner.x():
+					self.removeItem(self.dots[i])
+					del self.dots[i]
+			else:
+				break
+		
+		for i in range(len(self.stamps)):
+			if i < len(self.stamps):
+				x = self.stamps[i].x()
+				w = self.stamps[i].boundingRect().width()
+				if corner.x() > 0 and x+w < corner.x():
+					self.removeItem(self.stamps[i])
+					del self.stamps[i]
+			else:
+				break
+		
+		print len(self.dots), len(self.stamps)
+		
 	
 	
-	def setInterval(self, i):
+	def setCleanupInterval(self, i):
 		self.timer.setInterval(i)
 	
+	def setStampInterval(self, i):
+		self.stamptimer.setInterval(i)
 	
-	def time(self):
-		self.t = (time.time() - self.t0)*10
-		return self.t
+	def getTime(self):
+		return self.time
+		
+		
+	def makeDot(self, y, x=None, w=3, h=3, pen=blackpen):
+		x = x or self.t
+		d = self.addEllipse(x-w/2., y-h/2., w, h, pen)
+		d.setZValue(-1)
+		d.setOpacity(.6)
+		self.dots.append(d)
+		
+		
+	def makeStamp(self, y=0, x=None, z=0):
+		x = x or self.t
+		
+		t = "%2.1e" % int(self.time)
+		s = self.addText(t)
+		w = s.boundingRect().width()
+		
+		s.setPos(x-w/2, y)
+		s.adjustSize()
+		s.setZValue(z)
+		
+		if s.isObscured():
+			self.removeItem(s)
+		else:
+			self.stamps.append(s)
 	
 	
 	def addCurve(self, name, pen, startpoint=QPointF(0,0), title=None):
@@ -149,22 +198,15 @@ class Plotter (QGraphicsScene):
 		self.curves.update({name: curve})
 		
 	
-	def updateCurve(self, name, y, point=True):
+	def updateCurve(self, name, y, dot=True):
 		if self.curves.has_key(name):
-			if point is True:
-				self.makeDot(self.t, y)
+			if dot is True:
+				self.makeDot(y)
 				
 			self.__write(self.curves[name], y)
+		
+		
 	
-	def makeDot(self, x, y, w=2, h=2, pen=blackpen):
-		e = self.addEllipse(x-w/2., y-h/2., w, h, pen)
-		e.setZValue(-1)
-		e.setOpacity(.2)
-			
-
-				
-
-
 
 class graphtest (QMainWindow, Ui_Form):
 	
@@ -172,23 +214,19 @@ class graphtest (QMainWindow, Ui_Form):
 		super(graphtest, self).__init__(parent)
 		self.setupUi(self)
 		
-		self.scene = Plotter()
+		self.scene = Plotter(stretch=10,update=50)
 		self.graphicsView.setScene(self.scene)
 		self.graphicsView.scale(.9, .9)
 		
-		self.linespen = QPen()
-		self.linespen.setWidth(0)
-		self.linespen.setBrush(Qt.gray)
-		
 		self.scene.addCurve("green", greenpen)
-		self.scene.addCurve("blue", bluepen)
 		self.scene.addCurve("red", redpen)
+		self.scene.setStampInterval(5000)
 		
 		self.timer = QTimer()
-		self.timer.setInterval(1000)
+		self.timer.setInterval(500)
 		self.timer.start()
 		self.timer2 = QTimer()
-		self.timer2.setInterval(500)
+		self.timer2.setInterval(1000)
 		self.timer2.start()
 		
 		self.connect(self.timer, SIGNAL("timeout()"), self.genpoint)
@@ -197,18 +235,12 @@ class graphtest (QMainWindow, Ui_Form):
 		self.genpoint2()
 		
 	def genpoint(self):
-
 		yg = -(numpy.random.randint(128)+127)
-		
-		#yb = -(numpy.random.randint(128)+16)
-		#yg = -128
 		self.scene.updateCurve("green", yg)
 	
 	def genpoint2(self):
-		#yr = -64
 		yr = -(numpy.random.randint(128)+32)
-		self.scene.updateCurve("red", yr)
-		#self.scene.updateCurve("blue", yb)
+		self.scene.updateCurve("red", yr, False)
 		
 		
 	
