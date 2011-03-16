38c63909c16989f2b00c2f14feb009511d58419d
Remo Giermann <mo@liberejo.de>
2011-03-16 12:21:43 +0100

    GUI: more playing with graphs

--- gui/tinyPIDqt/scribble/graphtest.py
+++ gui/tinyPIDqt/scribble/graphtest.py
@@ -6,102 +6,139 @@ from graphtestui import *
 import time
 import numpy
 
+
+redpen = QPen()
+redpen.setBrush(Qt.red)
+redpen.setWidth(1)
+redpen.setJoinStyle(Qt.RoundJoin)
+redpen.setCapStyle(Qt.RoundCap)
+
+bluepen = QPen()
+bluepen.setBrush(Qt.blue)
+bluepen.setWidth(1)
+bluepen.setJoinStyle(Qt.RoundJoin)
+bluepen.setCapStyle(Qt.RoundCap)
+
+greenpen = QPen()
+greenpen.setBrush(Qt.green)
+greenpen.setWidth(1)
+greenpen.setJoinStyle(Qt.RoundJoin)
+greenpen.setCapStyle(Qt.RoundCap)
+
+
+class Curve(QGraphicsPathItem):
+	
+	def __init__(self, name, pen, *args, **kwargs):
+		super(Curve, self).__init__(QPainterPath(), *args, **kwargs)
+				
+		self.name = name
+		self.points = []
+		
+		self.setPen(pen)
+		self.setOpacity(.8)
+		
+		self.drawPath()
+	
+	
+	def drawPath(self):
+		
+		path = QPainterPath()
+		for p in self.points:
+			path.lineTo(p)
+		
+		self.setPath(path)
+		
+		
+	def addPoint(self, x, y):
+		self.points.append(QPointF(x, y))
+		self.drawPath()
+	
+	
+	def cropLeft(self, x):
+		if self.points[0].x() < x:
+			self.points = self.points[1:]
+	
+	
+	def cropRight(self, x):
+		if self.points[0].x() > x:
+			self.points = self.points[1:]
+		
+	
+	def count(self):
+		return len(self.points)
+	
+	
+	def length(self):
+		return self.path().length()
+		
+
+
 class graphtest (QMainWindow, Ui_Form):
 	
 	def __init__(self, parent=None):
 		super(graphtest, self).__init__(parent)
 		self.setupUi(self)
 		
-		self.timer = QTimer()
-		self.timer.setInterval(1000)
-		self.timer.start()
-		
-		self.connect(self.timer, SIGNAL("timeout()"), self.addpoint)
 		self.t0 = time.time()
+		self.t  = 0
 		
 		self.scene = QGraphicsScene()
 		self.graphicsView.setScene(self.scene)
 		self.graphicsView.scale(.9, .9)
 		
-		
 		self.linespen = QPen()
 		self.linespen.setWidth(0)
 		self.linespen.setBrush(Qt.gray)
 		
+		self.timer = QTimer()
+		self.timer.setInterval(500)
+		self.timer.start()
+		
+		self.connect(self.timer, SIGNAL("timeout()"), self.addpoint)
+		self.connect(self.timer, SIGNAL("timeout()"), self.time)
 	
-		self.redpen = QPen()
-		self.redpen.setBrush(Qt.red)
-		self.redpen.setWidth(1)
-		self.redpen.setJoinStyle(Qt.RoundJoin)
-		self.redpen.setCapStyle(Qt.RoundCap)
-		
-		self.bluepen = QPen()
-		self.bluepen.setBrush(Qt.blue)
-		self.bluepen.setWidth(1)
-		self.bluepen.setJoinStyle(Qt.RoundJoin)
-		self.bluepen.setCapStyle(Qt.RoundCap)
-		
-		self.greenpen = QPen()
-		self.greenpen.setBrush(Qt.green)
-		self.greenpen.setWidth(1)
-		self.greenpen.setJoinStyle(Qt.RoundJoin)
-		self.greenpen.setCapStyle(Qt.RoundCap)
-		
-	
-		greenpath = QPainterPath()
-		greenpath.moveTo(0,0)
-		redpath = QPainterPath()
-		redpath.moveTo(0,0)
-		bluepath = QPainterPath()
-		bluepath.moveTo(0,0)
-		
-		self.greenpathitem = self.scene.addPath(greenpath)
-		self.greenpathitem.setPath(greenpath)
-		self.greenpathitem.setPen(self.greenpen)
-		self.greenpathitem.setOpacity(.8)
-		
-		self.redpathitem = self.scene.addPath(redpath)
-		self.redpathitem.setPath(redpath)
-		self.redpathitem.setPen(self.redpen)
-		self.redpathitem.setOpacity(.8)
-
-		self.bluepathitem = self.scene.addPath(bluepath)
-		self.bluepathitem.setPath(bluepath)
-		self.bluepathitem.setPen(self.bluepen)
-		self.bluepathitem.setOpacity(.8)
-
+		self.curves = {}
 	
 	
+	def time(self):
+		self.t = (time.time() - self.t0) * 50
 	
 	def addpoint(self):
 	
-		greenpath = self.greenpathitem.path()
-		redpath = self.redpathitem.path()
-		bluepath = self.bluepathitem.path()
-				
 		yg = -(numpy.random.randint(128)+127)
 		yr = -(numpy.random.randint(128)+32)
 		yb = -(numpy.random.randint(128)+16)
 		
-		x = (time.time() - self.t0)*15
-		
-		greenpath.lineTo(x,yg)
-		bluepath.lineTo(x,yb)
-		redpath.lineTo(x,yr)
-		
-		self.greenpathitem.setPath(greenpath)
-		self.bluepathitem.setPath(bluepath)
-		self.redpathitem.setPath(redpath)
-		
-		self.graphicsView.centerOn(x+10, -128)
-		if int(x)%100 == 0:
-			self.scene.addLine(x,0,x,-255, self.linespen)
+		self.updateCurve("green", yg)
+		self.updateCurve("red", yr)
+		self.updateCurve("blue", yb)
 	
+	
+	def addCurve(self, name, pen, title=None):
+		
+		if self.curves.has_key(name):
+			raise KeyError("Curve with that name already exists")
+			return
+		
+		curve = Curve(name, pen)
+		self.scene.addItem(curve)
+		self.curves.update({name: curve})
+		
+	
+	def updateCurve(self, name, y):
+		if self.curves.has_key(name):
+			self.curves[name].addPoint(self.t, y)
+			self.curves[name].cropLeft(self.graphicsView.mapToScene(0,0).x())
+			self.graphicsView.centerOn(self.t, y)
+			print name, self.curves[name].count()
 
 
 if __name__ == "__main__":
 	import sys
 	app = QApplication(sys.argv)
 	d = graphtest()
+	d.addCurve("green", greenpen)
+	d.addCurve("blue", bluepen)
+	d.addCurve("red", redpen)
 	d.show()
 	app.exec_()
