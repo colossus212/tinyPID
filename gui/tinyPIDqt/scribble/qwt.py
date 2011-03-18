281b96654a39c97ca428a2e78a71c300083b21bd
Remo Giermann <mo@liberejo.de>
2011-03-18 01:24:28 +0100

    GUI: plotting tinyPID with Qwt. Perfect!

--- gui/tinyPIDqt/scribble/qwt.py
+++ gui/tinyPIDqt/scribble/qwt.py
@@ -14,30 +14,51 @@ class qwttest (QMainWindow, Ui_Form):
 		super(qwttest, self).__init__(parent)
 		self.setupUi(self)
 		
-		self.pid = tinyPID(port="/dev/ttyUSB1", baudrate=19200)
+		self.pid = tinyPID(port="/dev/ttyUSB2", baudrate=19200)
 		
 		
 		self.legend = QwtLegend(self.qwtPlot)
-		self.legend.setGeometry(self.qwtPlot.geometry())#QRect(10, 10, 501, 401))
+		self.legend.setGeometry(QRect(10, 10, 501, 401))
+		
+		gpen = QPen()
+		gpen.setBrush(QColor(150,150,150))
+		
+		wpen = QPen()
+		wpen.setBrush(QColor(0,180,0))
+		wpen.setWidth(2)
+		
+		xpen = QPen()
+		xpen.setBrush(QColor(180,0,0))
+		xpen.setWidth(2)
+		
+		ypen = QPen()
+		ypen.setBrush(QColor(0,0,180))
+		ypen.setWidth(2)
+		
+		
+		self.grid = QwtPlotGrid()
+		self.grid.setMajPen(gpen)
+		self.grid.attach(self.qwtPlot)
+		self.gridEnable(False)
 		
 		self.curve_w = QwtPlotCurve("Setpoint")
 		self.curve_w.attach(self.qwtPlot)
-		self.curve_w.setPen(Qt.green)
+		self.curve_w.setPen(wpen)
 		self.curve_w.updateLegend(self.legend)
 		
 		self.curve_y = QwtPlotCurve("Output")
 		self.curve_y.attach(self.qwtPlot)
-		self.curve_y.setPen(Qt.blue)
+		self.curve_y.setPen(ypen)
 		self.curve_y.updateLegend(self.legend)
 		
 		self.curve_x = QwtPlotCurve("Process Value")
 		self.curve_x.attach(self.qwtPlot)
-		self.curve_x.setPen(Qt.red)
+		self.curve_x.setPen(xpen)
 		self.curve_x.updateLegend(self.legend)
 		
+		self.qwtPlot.setAxisScale(QwtPlot.yLeft, 0, 300)
 		self.qwtPlot.setAxisTitle(QwtPlot.yLeft, "")
 		self.qwtPlot.setAxisTitle(QwtPlot.xBottom, "t/s")
-		self.qwtPlot.setAxisScale(QwtPlot.yLeft, 0, 255)
 		self.qwtPlot.replot()
 		
 		self.t0 = time.time()
@@ -45,23 +66,59 @@ class qwttest (QMainWindow, Ui_Form):
 		self.y = []
 		self.w = []
 		self.x = []
-		self.hist = 5
 		
+		self.view = 10
 		self.axmin = 0
-		self.axmax = self.hist
-		self.step = 0.3
+		self.axmax = self.view
+		self.step = 0
 		
 		self.timer = QTimer()
-		self.timer.setInterval(50)
+		self.timer.setInterval(150)
 		self.timer.start()
-		#self.timer2 = QTimer()
-		#self.timer2.setInterval(1000)
-		#self.timer2.start()
 		
 		self.connect(self.timer, SIGNAL("timeout()"), self.genpoint)
-		#self.connect(self.timer2, SIGNAL("timeout()"), self.genpoint2)
 		self.genpoint()
-		#self.genpoint2()
+		
+	def gridEnable(self, enable):
+		self.grid.enableX(enable)
+		self.grid.enableY(enable)
+	
+	
+	def setView(self, length):
+		if length > self.view:
+			self.axmax = self.axmin + length
+			self.view = length
+		elif length < self.view:
+			self.axmin = self.t[-2]
+			self.axmax = length + self.axmin
+			self.view = length
+		else:
+			return
+		
+		self.qwtPlot.setAxisScale(QwtPlot.xBottom, self.axmin, self.axmax)
+
+
+	def setScroll(self, percent):
+		self.step = percent/100.
+
+
+	def setYmin(self, ymin):
+		ymax = self.ymaxSpinBox.value()
+		self.qwtPlot.setAxisScale(QwtPlot.yLeft, ymin, ymax)
+
+
+	def setYmax(self, ymax):
+		ymin = self.yminSpinBox.value()
+		self.qwtPlot.setAxisScale(QwtPlot.yLeft, ymin, ymax)
+	
+	
+	def setAutoscale(self, enable):
+		if enable:
+			self.qwtPlot.setAxisAutoScale(QwtPlot.yLeft)
+		else:
+			ymin, ymax = self.yminSpinBox.value(), self.ymaxSpinBox.value()
+			self.qwtPlot.setAxisScale(QwtPlot.yLeft, ymin, ymax)
+			
 		
 	def genpoint(self):
 		self.t.append(time.time()-self.t0)
@@ -82,12 +139,13 @@ class qwttest (QMainWindow, Ui_Form):
 			else:
 				break
 		
-		if self.t[-1] > self.axmax:
-			self.axmin = self.axmin + self.hist*self.step
-			self.axmax = self.axmax + self.hist*self.step
+		if self.t[-1] > self.axmax and self.step > 0:
+			self.axmin = self.axmin + self.view*self.step
+			self.axmax = self.axmax + self.view*self.step
 			self.qwtPlot.setAxisScale(QwtPlot.xBottom, self.axmin, self.axmax)
 			
-		print "Points:", len(self.t), len(self.w,), len(self.x), len(self.y)
+		print "Points:", len(self.t)+len(self.w,)+len(self.x)+len(self.y)
+		print "AxBounds:", self.axmin, self.axmax
 		self.qwtPlot.replot()
 	
 	def genpoint2(self):
--- gui/tinyPIDqt/scribble/qwtui.py
+++ gui/tinyPIDqt/scribble/qwtui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'qwt.ui'
 #
-# Created: Thu Mar 17 16:48:34 2011
+# Created: Fri Mar 18 00:45:27 2011
 #      by: PySide uic UI code generator
 #
 # WARNING! All changes made in this file will be lost!
@@ -10,19 +10,91 @@
 from PyQt4 import QtCore, QtGui
 from PyQt4.Qwt5 import *
 
+
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(520, 418)
-        self.qwtPlot = QwtPlot(Form)
-        self.qwtPlot.setGeometry(QtCore.QRect(10, 10, 501, 401))
+        Form.resize(600, 520)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
+        Form.setMinimumSize(QtCore.QSize(600, 520))
+        Form.setMaximumSize(QtCore.QSize(600, 520))
+        self.widget = QtGui.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(11, 11, 571, 491))
+        self.widget.setObjectName("widget")
+        self.verticalLayout = QtGui.QVBoxLayout(self.widget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.gridCheckBox = QtGui.QCheckBox(self.widget)
+        self.gridCheckBox.setObjectName("gridCheckBox")
+        self.horizontalLayout.addWidget(self.gridCheckBox)
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.autoscaleCheckBox = QtGui.QCheckBox(self.widget)
+        self.autoscaleCheckBox.setChecked(False)
+        self.autoscaleCheckBox.setObjectName("autoscaleCheckBox")
+        self.horizontalLayout.addWidget(self.autoscaleCheckBox)
+        self.yminSpinBox = QtGui.QSpinBox(self.widget)
+        self.yminSpinBox.setAccelerated(True)
+        self.yminSpinBox.setMinimum(-9999)
+        self.yminSpinBox.setMaximum(9999)
+        self.yminSpinBox.setObjectName("yminSpinBox")
+        self.horizontalLayout.addWidget(self.yminSpinBox)
+        self.ymaxSpinBox = QtGui.QSpinBox(self.widget)
+        self.ymaxSpinBox.setMinimum(-9999)
+        self.ymaxSpinBox.setMaximum(9999)
+        self.ymaxSpinBox.setProperty("value", QtCore.QVariant(300))
+        self.ymaxSpinBox.setObjectName("ymaxSpinBox")
+        self.horizontalLayout.addWidget(self.ymaxSpinBox)
+        spacerItem2 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem2)
+        self.viewLabel = QtGui.QLabel(self.widget)
+        self.viewLabel.setObjectName("viewLabel")
+        self.horizontalLayout.addWidget(self.viewLabel)
+        self.viewSpinBox = QtGui.QSpinBox(self.widget)
+        self.viewSpinBox.setMinimum(1)
+        self.viewSpinBox.setMaximum(120)
+        self.viewSpinBox.setProperty("value", QtCore.QVariant(10))
+        self.viewSpinBox.setObjectName("viewSpinBox")
+        self.horizontalLayout.addWidget(self.viewSpinBox)
+        self.stepLabel = QtGui.QLabel(self.widget)
+        self.stepLabel.setObjectName("stepLabel")
+        self.horizontalLayout.addWidget(self.stepLabel)
+        self.scrollSpinBox = QtGui.QSpinBox(self.widget)
+        self.scrollSpinBox.setAccelerated(True)
+        self.scrollSpinBox.setMaximum(100)
+        self.scrollSpinBox.setObjectName("scrollSpinBox")
+        self.horizontalLayout.addWidget(self.scrollSpinBox)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.qwtPlot = QwtPlot(self.widget)
         self.qwtPlot.setObjectName("qwtPlot")
+        self.verticalLayout.addWidget(self.qwtPlot)
 
         self.retranslateUi(Form)
+        QtCore.QObject.connect(self.gridCheckBox, QtCore.SIGNAL("toggled(bool)"), Form.gridEnable)
+        QtCore.QObject.connect(self.scrollSpinBox, QtCore.SIGNAL("valueChanged(int)"), Form.setScroll)
+        QtCore.QObject.connect(self.viewSpinBox, QtCore.SIGNAL("valueChanged(int)"), Form.setView)
+        QtCore.QObject.connect(self.autoscaleCheckBox, QtCore.SIGNAL("toggled(bool)"), self.yminSpinBox.setHidden)
+        QtCore.QObject.connect(self.autoscaleCheckBox, QtCore.SIGNAL("toggled(bool)"), self.ymaxSpinBox.setHidden)
+        QtCore.QObject.connect(self.autoscaleCheckBox, QtCore.SIGNAL("toggled(bool)"), Form.setAutoscale)
+        QtCore.QObject.connect(self.yminSpinBox, QtCore.SIGNAL("valueChanged(int)"), Form.setYmin)
+        QtCore.QObject.connect(self.ymaxSpinBox, QtCore.SIGNAL("valueChanged(int)"), Form.setYmax)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "QWT Plotter", None, QtGui.QApplication.UnicodeUTF8))
+        self.gridCheckBox.setText(QtGui.QApplication.translate("Form", "Grid", None, QtGui.QApplication.UnicodeUTF8))
+        self.autoscaleCheckBox.setText(QtGui.QApplication.translate("Form", "Autoscale", None, QtGui.QApplication.UnicodeUTF8))
+        self.viewLabel.setText(QtGui.QApplication.translate("Form", "View", None, QtGui.QApplication.UnicodeUTF8))
+        self.viewSpinBox.setSuffix(QtGui.QApplication.translate("Form", "s", None, QtGui.QApplication.UnicodeUTF8))
+        self.stepLabel.setText(QtGui.QApplication.translate("Form", "Scroll", None, QtGui.QApplication.UnicodeUTF8))
+        self.scrollSpinBox.setSuffix(QtGui.QApplication.translate("Form", "%", None, QtGui.QApplication.UnicodeUTF8))
 
 #from qwt_plot import QwtPlot
 
