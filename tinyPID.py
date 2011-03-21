0d216f1b39d584947c85e80dd8f11bbb06ee7243
Remo Giermann <mo@liberejo.de>
2011-03-21 14:28:29 +0100

    tinyPID.py: wrote some docstrings and added log(), info() from tools.py

--- tinyPID.py
+++ tinyPID.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
 #
-#
-# Frontend to tinyPID
+# Frontend to tinyPID.
 #
 # author: Remo Giermann (mo@liberejo.de)
 # created: 2011/03/03
@@ -65,11 +65,72 @@ def dfactor_unscale(f):
 
 
 class tinyPID (object):
+	"""
+	Frontend to tinyPID.
+	
+	
+	Methods:
+	--------
+	auto():          set controller to automatic mode
+	manual():        set controller to manual mode
+	save():          save parameters to device's EEPROM
+	
+	get_mode():      return the operation mode of the controller
+	get_constants(): get calculation constants scaling factor and sampling time
+	                  used on the controller
+	get_scale():     return the process value scaling limits pvmin, pvmax
+	get_limits():    return the output limits
+	
+	get_Kd():        return the differential factor of the PID controller
+	get_Ki():        return the integral factor 
+	get_Kp():        return the proportional factor
+	
+	get_output():    return the current output reading
+	get_pv():        return the current process value reading
+	get_setpoint():  return the setpoint
+	
+	set_Kp(k):       set PID proportional factor 
+	set_Ki(k):       set PID integral factor 
+	set_Kd(k):       set PID differential factor 
+	
+	set_limits(min, max):
+	                 set output limits to 'min' and 'max'
+	set_scale(min, max):
+	                 set process value scale limits to 'min', 'max'.
+					  The controller rescales the PV to fullscale 0…255.
+						  
+	set_output(y):   set output value to 'y' (this toggles manual mode internally)
+	set_setpoint(w): set the setpoint to 'w'
+	
+	info():          print out most of the PID data
+	log(interval):   continuously get and print setpoint, 
+					  process value, output and error
+
+	Attributes:
+	-----------
+	Kp, Ki, Kd: get/set PID factors
+	Tv, Tn:     get/set PID time constants (same effect as Kd, Ki)
+	
+	w, x, y, e: get/set setpoint, process value, output, error
+	SP, PV:     get/set setpoint, process value
+	opmode:     get/set operation mode ('a', or 'm')
+	
+	xmin, xmax, pvmin, pvmax:
+	            get/set process value scale limits
+	ymin, ymax, outmin, outmax:
+	            get/set output limits
+	
+	"""
+
 
 	def __init__(self, *args, **kwargs):
+		"""
+		Arguments are directly passed to serial.Serial(). Should need at least 'port' and 'baudrate'.
+		"""
 		self.com = serial.Serial(*args, **kwargs)
 		if not self.com.getTimeout():
 			self.com.setTimeout(2)
+	
 	
 	def __del__(self):
 		del self.com
@@ -83,6 +144,7 @@ class tinyPID (object):
 		else:
 			return s	
 	
+	
 	def __readb(self, count=1):
 		""" Read byte(s) and return integer(s). """
 		s = self.__readc(count)
@@ -94,7 +156,8 @@ class tinyPID (object):
 				return b
 		else:
 			return None
-			
+	
+	
 	def __readw(self, count=1):
 		""" Read byte pair(s) MSB, LSB and return integer(s). """
 		r = []
@@ -110,6 +173,7 @@ class tinyPID (object):
 		else:
 			return r
 
+
 	def __write(self, *args):
 		""" Write characters/bytes. """
 		for s in args:
@@ -119,6 +183,7 @@ class tinyPID (object):
 				s = chr(s)
 			self.com.write(s)
 
+
 	def __writew(self, word):
 		""" Write a long number as MSB/LSB-pair. """
 		msb, lsb = to_bytes(word)
@@ -127,121 +192,147 @@ class tinyPID (object):
 
 	def get_mode(self):
 		""" Get operation mode. """
-		
 		self.__write("gm")
 		return self.__readc()
 
+
 	def get_Kp(self):
 		""" Get proportional factor. """
-		
 		self.__write("gp")
 		return pfactor_unscale(self.__readw())
 
+
 	def get_Ki(self):
 		""" Get integral factor. """
-		
 		self.__write("gi")
 		return ifactor_unscale(self.__readw())
-        
+
+
 	def get_Kd(self):
 		""" Get derivative factor. """
-		
 		self.__write("gd")
 		return dfactor_unscale(self.__readw())
 
+
 	def get_pv(self):
 		""" Get process value 'x'. """
-		
 		self.__write("gx")
 		return self.__readb()
 
+
 	def get_setpoint(self):
 		""" Get setpoint 'w'. """
-		
 		self.__write("gv")
 		return self.__readb()
 
+
 	def get_output(self):
 		""" Get output value 'y'. """
-		
 		self.__write("gy")
 		return self.__readb()
 
+
 	def get_limits(self):
 		""" Get output limits. """
-		
 		self.__write("gl")
 		return self.__readb(2)
 
+
 	def get_scale(self):
 		""" Get PV scale. """
-		
 		self.__write("gs")
 		xmin, xmax = self.__readb(2)
 		xscale = self.__readw()
 		return xmin, xmax
 
+
 	def get_constants(self):
 		""" Get calculation constants sampling time and scaling factor. """
-		
 		self.__write("gc")
 		return self.__readb(2)
 
+
 	def set_Kp(self, Kp):
 		""" Set proportional factor 'Kp'. """
-		
 		self.__write("sp")
 		self.__writew(pfactor_scale(Kp))
-        
+
+
 	def set_Ki(self, Ki):
 		""" Set integral factor 'Ki'. """
-		
 		self.__write("si")
 		self.__writew(ifactor_scale(Ki))
-	
+
+
 	def set_Kd(self, Kd):
 		""" Set derivative factor 'Kd'. """
-		
 		self.__write("sd")
 		self.__writew(dfactor_scale(Kd))
-	
+
+
 	def set_setpoint(self, w):
 		""" Set setpoint 'w'. """
-		
 		self.__write("sv", w)
+
 
 	def set_output(self, y):
 		""" Set output value 'y' manually. """
-		
 		self.__write("sy", y)
-		
+
+
 	def set_limits(self, ymin=0, ymax=255):
 		""" Set output limits. """
-		
 		self.__write("sl", ymin, ymax)
+
 
 	def set_scale(self, xmin=0, xmax=255):
 		""" Set PV scale. """
-		
 		self.__write("ss", xmin, xmax)
 		self.__writew(SCALING_FACTOR * 255/(xmax-xmin))
 
+
 	def auto(self):
 		""" Set to automatic mode. """
-		
 		self.__write("a")
+
 
 	def manual(self):
 		""" Set to manual mode. """
-		
 		self.__write("m")
+
 
 	def save(self):
 		""" Save device configuration to EEPROM. """
-		
 		self.__write("e")
 
 
+	def info(self):
+		""" Print PID data. """
+		p, i, d = self.Kp, self.Ki, self.Kd
+		v, n = Kd_to_Tv(p, d), Ki_to_Tn(p, i)
+		w, x, y = self.w, self.x, self.y
+		ymin, ymax = self.get_limits();
+		xmin, xmax = self.get_scale();
+		e = w - x
+		m = self.opmode
+		
+		print "Kp: %2.2f \nKi: %2.2f (Tn: %2.2f) \nKd: %2.2f (Tv: %2.2f)" % (p, i, n, d, v)
+		print "w: %i \nx: %i (%i..%i) \ne: %i \ny: %i (%i..%i)" % (w, x, xmin, xmax, e, y, ymin, ymax)
+		print m == 'a' and "automatic" or "manual"
+
+
+	def log(self, interval=1):
+		""" Print out w, x, y, e continuously in an interval of 'interval'. """
+		try:
+			while True:
+				w, x, y = self.w, self.x, self.y
+				e = w - x
+				print "w: %3i, x: %3i, y: %3i, e: %3i" % (w, x, y, e)
+				sleep(interval)
+		except KeyboardInterrupt:
+			return
+	
+	
 	def __getattr__(self, name):
 		if name == "Kp":
 			return self.get_Kp()
@@ -310,10 +401,8 @@ class tinyPID (object):
 				self.auto()
 			elif value == 'm':
 				self.manual()
-			elif value == 'o':
-				self.stop()
 			else:
-				raise AttributeError("opmode must be 'a', 'm' or 'o'")
+				raise AttributeError("opmode must be 'a' or 'm'")
 			
 		elif name in ("ymax, ymin, outmax, outmin"):
 			ymin, ymax = self.get_limits()
